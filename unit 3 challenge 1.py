22df31af3b35f3fef5c58962aa01dbc7b833fc2b
Naveenkathir2004 <144199202+Naveenkathir2004@users.noreply.github.com>
2023-09-21 17:01:42 +0530

    Add files via upload

---
+++ unit 3 challenge 1.py
@@ -0,0 +1,15 @@
+def linear_search_product(product_list, target_product):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(index)
+    return indices
+
+# Example usage:
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes","cherry","Apple"]
+target= "Apple"
+result = linear_search_product(products, target)
+print(result)  # Output will be [0, 3] because "Apple" is found at indices 0 and 3.
+target2="mango"
+result = linear_search_product(products, target2)
+print(result)# output will be empty list[] because "mango" is not found in the list of products 
